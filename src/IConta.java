35fb37fbed6931ba247cf1133ec52648c0c8ead7
lmcorazolla <mateus.corazolla@gmail.com>
2022-03-03 11:12:55 -0300

    escrevendo o menu de forma provavelmente burra

--- src/IConta.java
+++ src/IConta.java
@@ -9,4 +9,6 @@ public interface IConta {
     void transferir(double valor, IConta contaDestino);
 
     void imprimirExtrato();
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main implements IConta {
 
     public static void main(String[] args) {
+        // Criando dois clientes
         Cliente luiz = new Cliente();
         luiz.setNome("Luiz Mateus");
 
@@ -22,8 +23,75 @@ public class Main {
         correnteGiullia.transferir(8000, poupancaGiullia);
 
         Scanner teclado = new Scanner(System.in);
+        int opcaoMenu;
 
 
+        System.out.println("Bem vindo ao Banco! \nSelecione a sua opção:");
+        System.out.println("1: Acessar Conta Luiz  2: Acessar Conta Giullia");
+        opcaoMenu = teclado.nextInt();
+        if (opcaoMenu == 1) {
+            System.out.println("1: Ver saldo Corrente  2: Ver saldo Poupança");
+            System.out.println("3: Depositar Corrente  4: Depositar Poupança");
+            System.out.println("5: Sacar Corrente  6: Sacar Poupança");
+            System.out.println("7: Transferir Corrente  8: Transferir Poupança");
+            opcaoMenu = teclado.nextInt();
+            if (opcaoMenu == 1) correnteLuiz.imprimirExtrato();
+            else if (opcaoMenu == 2) poupancaLuiz.imprimirExtrato();
+            else if (opcaoMenu == 3) {
+                System.out.print("Valor Depósito: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                correnteLuiz.depositar(valor);
+            } else if (opcaoMenu == 4) {
+                System.out.print("Valor Depósito: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                poupancaLuiz.depositar(valor);
+            } else if (opcaoMenu == 5) {
+                System.out.print("Valor Saque: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                correnteLuiz.sacar(valor);
+            } else if (opcaoMenu == 6) {
+                System.out.print("Valor Saque: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                poupancaLuiz.sacar(valor);
+            } else if (opcaoMenu == 7) {
+                IConta contaDestino = null;
+                System.out.print("Conta Destino: ");
+
+                System.out.print("Valor Transferência: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                correnteLuiz.transferir(valor, contaDestino);
+            }
+
+
+        } else if (opcaoMenu == 2) {
+            System.out.println("1: Ver saldo Corrente  2: Ver saldo Poupança");
+            System.out.println("3: Depositar Corrente  4: Depositar Poupança");
+            System.out.println("5: Sacar Corrente  6: Sacar Poupança");
+            System.out.println("7: Transferir Corrente  8: Transferir Poupança");
+            opcaoMenu = teclado.nextInt();
+            if (opcaoMenu == 1) correnteGiullia.imprimirExtrato();
+            else if (opcaoMenu == 2) poupancaGiullia.imprimirExtrato();
+            else if (opcaoMenu == 3) {
+                System.out.print("Valor Depósito: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                correnteGiullia.depositar(valor);
+            } else if(opcaoMenu == 4){
+                System.out.print("Valor Depósito: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                poupancaGiullia.depositar(valor);
+            } else if(opcaoMenu == 5){
+                System.out.print("Valor Saque: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                correnteGiullia.sacar(valor);
+            } else if(opcaoMenu == 6){
+                System.out.print("Valor Saque: R$ ");
+                double valor = Double.parseDouble(teclado.next());
+                poupancaGiullia.sacar(valor);
+            }
+
+        } else if (opcaoMenu > 2 || opcaoMenu <= 0) {
+            System.out.println("Opção inválida.");
+        }
 
 
 
@@ -36,4 +104,23 @@ public class Main {
 
     }
 
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, IConta contaDestino) {
+
+    }
+
+    @Override
+    public void imprimirExtrato() {
+
+    }
 }
